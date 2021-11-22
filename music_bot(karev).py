5ebfcb8af429b959d6d29f638c3abb8a1ecf3b39
Synx46 <91374682+Synx46@users.noreply.github.com>
2021-11-22 19:16:56 +0530

    Create music_bot(karev).py

---
+++ music_bot(karev).py
@@ -0,0 +1,79 @@
+import discord
+from discord.ext import commands, tasks
+import youtube_dl
+
+from random import choice
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+client = commands.Bot(command_prefix=';')
+
+token = "#"
+
+status = ['Pop Music', 'Botflix', 'Bot Theft Auto 6', 'Botney Spears']
+
+
+@client.event
+async def on_ready():
+    change_status.start()
+    print("Bot is online!")
+
+
+@client.command(name='hello', help=': Returns a random welcome message')
+async def hello(ctx):
+    responses = ['***grumble*** Why did you wake me up?', 'Top of the morning', 'I couldve done so much more', 'I was having a great day until now']
+    await ctx.send(choice(responses))
+
+
+
+@tasks.loop(seconds=60)
+async def change_status():
+    await client.change_presence(activity=discord.Game(choice(status)))
+
+client.run(token)
+
+
+
+
